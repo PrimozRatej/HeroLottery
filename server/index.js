8252d7818963aac9f236814ba0762bac07e3a82e
Benjamin <beny@pop-os.localdomain>
2021-12-02 21:31:56 +0100

    testitng

--- server/index.js
+++ server/index.js
@@ -1,52 +1,73 @@
-const MySQlController = require('./controllers/MySQlController');
-const SocketController = require('./controllers/SocketController');
-require('dotenv').config({ path: '../.env' });
+require("dotenv").config({ path: "../.env" });
+var mysql = require("mysql");
+var get = require("axios");
 
-const env = setEvn();
+var host = process.env.MYSQL_HOST;
+var user = process.env.MYSQL_ROOT_USERNAME;
+var password = process.env.MYSQL_ROOT_PASSWORD;
+var database = process.env.MYSQL_DATABASE;
+var port = process.env.MYSQL_DOCKER_PORT;
+console.log(user);
+console.log(password);
+class RaffleDTO {
+  lotteryNumber;
+  createdAt;
+  constructor(lotteryNumber, createdAt) {
+    this.lotteryNumber = lotteryNumber;
+    this.createdAt = createdAt;
+  }
 
-const socket = SocketController.build();
-const mysql = MySQlController.build(env);
-
-const program = async () => {
-
-    socket.start();
-
-    mysql.eventListener.start();
-
-    socket.onClientConnect(mysql.queries.getScore);
-
-    socket.onClientAttendRaffle(mysql.functions.userAttendRaffle);
-
-    mysql.eventListener.addTrigger(
-        mysql.tableUpdateTrigger('user_raffle', async function () {
-            var score = await mysql.queries.getScore();
-            socket.emitScore(score);
-        })
-    );
-
-    mysql.eventListener.addTrigger(
-        mysql.tableInsertTrigger('raffle', async function () {
-            var score = await mysql.queries.getScore();
-            socket.emitScore(score);
-        })
-    );
-};
-program()
-    .then(() => console.log('\u001b[' + 32 + 'm' + 'Process started...' + '\u001b[0m'))
-    .catch(() => {
-        console.log(error);
-    });
-
-function setEvn() {
-    return {
-        MYSQL_ROOT_USERNAME: process.env.MYSQL_ROOT_USERNAME,
-        MYSQL_ROOT_PASSWORD: process.env.MYSQL_ROOT_PASSWORD,
-        MYSQL_ADMIN_USERNAME: process.env.MYSQL_ADMIN_USERNAME,
-        MYSQL_ADMIN_PASSWORD: process.env.MYSQL_ADMIN_PASSWORD,
-        MYSQL_DATABASE: process.env.MYSQL_DATABASE,
-        MYSQL_HOST: process.env.MYSQL_HOST,
-        MYSQL_LOCAL_PORT: process.env.MYSQL_LOCAL_PORT,
-        MYSQL_DOCKER_PORT: process.env.MYSQL_DOCKER_PORT,
-        NODEJS_LOCAL_PORT: process.env.NODEJS_LOCAL_PORT
-    }
+  static fromJson(json) {
+    return Object.assign(new RaffleDTO(), json);
+  }
 }
+
+// takes the docker name as host
+var connection = mysql.createConnection({
+  host: "herolottery_mysqldb_1",
+  user: user,
+  password: password,
+  database: database,
+  port: port,
+});
+
+console.log(connection.state);
+
+setInterval(() => {
+  get("https://celtra-lottery.herokuapp.com/api/getLotteryNumber")
+    .then((response) => {
+      dto = RaffleDTO.fromJson(response.data);
+      var insertRafflesql =
+        "INSERT INTO raffle (lottery_number, created_at) VALUES ('" +
+        dto.lotteryNumber +
+        "', '" +
+        dto.createdAt +
+        "')";
+      connection.query(insertRafflesql, function (err, result) {
+        // duplicated raffle
+        if (err?.errno === 1062) {
+          return;
+        } else if (err) throw Error(err);
+        else {
+          connection.query(
+            `SELECT id FROM raffle WHERE created_at = \"${dto.createdAt}\";`,
+            function (err, result) {
+              if (err) throw err;
+              var setUsersToRaffleSql = `UPDATE user_raffle SET raffle_id = ${result[0].id} WHERE raffle_id IS NULL;`;
+              connection.query(setUsersToRaffleSql, function (err, result) {
+                // duplicated raffle
+                if (err?.errno === 1062) {
+                  return;
+                } else if (err) throw Error(err);
+                else console.log("\u001b[1;32m CREATED \u001b[0m", dto);
+              });
+            }
+          );
+          console.log("\u001b[1;32m CREATED \u001b[0m", dto);
+        }
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}, 1000);
